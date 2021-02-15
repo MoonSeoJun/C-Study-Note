764458c2bb4c31757f2b5a9e465e07a916a29321
MunSeoJun <msj00130@gmail.co>
2021-02-15 21:49:40 +0900

    C# setup & Print self-introduction

---
+++ C#_Console/First_Console/First_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Moon";
+            int characterAge = 18;
+
+            Console.WriteLine("Hello, my name is " + characterName);
+            Console.WriteLine("I am " + characterAge + " years old");
+        }
+    }
+}
