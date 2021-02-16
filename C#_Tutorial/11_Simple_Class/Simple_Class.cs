3a6598c84b6d0b75807b1143acc6441682358e7c
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:43:28 +0900

    Simple class EX

---
+++ C#_Tutorial/11_Simple_Class/Simple_Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book subBook = new Book();
+
+            subBook.title = "BBC";
+            subBook.author = "me";
+            subBook.pages = 14;
+
+            Console.WriteLine(subBook.title);
+            Console.WriteLine(subBook.author);
+            Console.WriteLine(subBook.pages);
+        }
+    }
+}
