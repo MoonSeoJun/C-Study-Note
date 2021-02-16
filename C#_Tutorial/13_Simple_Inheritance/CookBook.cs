de37e10430a5af8d1acab78795cb5182e69fcdec
MunSeoJun <msj00130@gmail.co>
2021-02-16 17:02:15 +0900

    Inheritance EX

---
+++ C#_Tutorial/13_Simple_Inheritance/CookBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class CookBook : Book
+    {
+        private string Genre;
+        private string Content;
+
+        public string GenreGS
+        {
+            get { return Genre; }
+            set { Genre = value; }
+        }
+
+        public string ContentGS
+        {
+            get { return Content; }
+            set { Content = value; }
+        }
+    }
+}
---
+++ C#_Tutorial/13_Simple_Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CookBook cookBook = new CookBook();
+
+            cookBook.TitleGS = "WonwuDad";
+            cookBook.AuthorGS = "Wonwu";
+            cookBook.PagesGS = 15;
+            cookBook.GenreGS = "Cook";
+            cookBook.ContentGS = "bla bla bla";
+
+            Console.WriteLine(cookBook.TitleGS);
+            Console.WriteLine(cookBook.AuthorGS);
+            Console.WriteLine(cookBook.PagesGS);
+            Console.WriteLine(cookBook.GenreGS);
+            Console.WriteLine(cookBook.ContentGS);
+        }
+    }
+}
