763d638063b242977394cc2e76f5f1d92413dada
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:53:25 +0900

    Getter Setter

---
+++ C#_Tutorial/12_Simple_Getter_Setter/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+
+        public string TitleGS
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string AuthorGS
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public int PagesGS
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
+    }
+}
---
+++ C#_Tutorial/12_Simple_Getter_Setter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book subBook = new Book();
+
+            subBook.TitleGS = "BBC";
+            subBook.AuthorGS = "Moon";
+            subBook.PagesGS = 14;
+
+            Console.WriteLine(subBook.TitleGS);
+            Console.WriteLine(subBook.AuthorGS);
+            Console.WriteLine(subBook.PagesGS);
+        }
+    }
+}
