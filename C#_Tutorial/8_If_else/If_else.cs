1a9174c04c11cef1530a2ee507448d05d65fb351
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:20:31 +0900

    If else EX
    2020/02/16

---
+++ C#_Tutorial/8_If_else/If_else.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool smart = true;
+
+            if (smart)
+            {
+                Console.WriteLine("You are smart");
+            }
+            else
+            {
+                Console.WriteLine("You are stupid");
+            }
+        }
+    }
+}
