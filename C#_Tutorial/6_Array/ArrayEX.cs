37457a296eeefd493d63a5565b7ae45ea692a930
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:06:17 +0900

    Array EX

---
+++ C#_Tutorial/6_Array/ArrayEX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Nums = { 5, 6, 8, 3, 4 };
+            string[] family = new string[4];
+            family[0] = "me";
+            family[1] = "mom";
+            family[2] = "bro";
+            family[3] = "dad";
+
+            Console.WriteLine(Nums[0]);
+            Console.WriteLine(family[0]);
+        }
+    }
+}
