fb50daa0ed3f141c0e86201528cf2ec9952b7121
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:11:54 +0900

    Method EX
    2021/02/16

---
+++ C#_Tutorial/7_Methods/MethodEX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Moon", 18);
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
