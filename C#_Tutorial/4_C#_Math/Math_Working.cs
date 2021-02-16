9820180527595f734ee62d6e4d44f108d59396cc
MunSeoJun <msj00130@gmail.co>
2021-02-16 15:37:23 +0900

    Math Work

---
+++ C#_Tutorial/4_C#_Math/Math_Working.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(4, 3)); // 4의 3제곱
+            Console.WriteLine(Math.Sqrt(36)); // 해당값의 제곱근을 구함
+            Console.WriteLine(Math.Max(4, 90)); // 최대값을 구함
+            Console.WriteLine(Math.Min(4, 90)); // 최소값을 구함
+            Console.WriteLine(Math.Round(5.5)); // 가장 가까운 자연수를 구함
+        }
+    }
+}
