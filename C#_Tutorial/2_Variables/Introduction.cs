01b4bbf41592eef37b157380c833d0ef36f23c3c
MunSeoJun <msj00130@gmail.co>
2021-02-16 14:55:59 +0900

    Variables

---
+++ C#_Tutorial/2_Variables/Introduction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "Moon";
+            int CharacterAge = 18;
+
+            Console.WriteLine("Hello, my name is " + CharacterName);
+            Console.WriteLine("I am " + CharacterAge + " years old");
+        }
+    }
+}
