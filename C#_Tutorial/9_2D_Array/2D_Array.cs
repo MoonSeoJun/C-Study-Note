a42e1778d4816c42732f86b4aa3617665543f8d8
MunSeoJun <msj00130@gmail.co>
2021-02-16 16:29:33 +0900

    2D Array

---
+++ C#_Tutorial/9_2D_Array/2D_Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] number_2d = { { 0, 3 }, { 1, 2 }, { 1, 1 } };
+
+            Console.WriteLine(number_2d[0, 0]);
+            Console.WriteLine(number_2d[1, 1]);
+            Console.WriteLine(number_2d[1, 1]);
+        }
+    }
+}
