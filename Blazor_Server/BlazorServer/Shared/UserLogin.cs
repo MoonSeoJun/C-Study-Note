21aac68f771fce2ad32caeb6012edfe3e2323981
MunSeoJun <msj00130@gmail.com>
2021-02-19 18:14:34 +0900

    [Add] Login page

---
+++ Blazor_Server/BlazorServer/Shared/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorServer.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Pleaze enter a Username.")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
